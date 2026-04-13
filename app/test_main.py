d89eee2e2c54189ff68535dc6b18929cc8cde1e2
Sara <radosavljevic.sara@gmail.com>
2026-04-13 10:18:47 +0200

    Add test

--- app/test_main.py
+++ app/test_main.py
@@ -1,2 +1,12 @@
 def test_dummy():
     assert 1 == 1
+
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
